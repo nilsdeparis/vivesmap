5517564961807cda1ee0cf3f2b611b8f4bc888e9
nilsdeparis <72404130+nilsdeparis@users.noreply.github.com>
2020-10-29 18:28:58 +0100

    Update and rename abc.php to index.php

--- index.php
+++ index.php
@@ -59,7 +59,7 @@ $_SESSION['garderOuvert'] = false;
 		<meta name="viewport" content="width=device-width">
 		<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;600;700;800;900&display=swap" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css2?family=Varela+Round&display=swap" rel="stylesheet">
-		<link href="abc.css" rel="stylesheet" type="text/css" />
+		<link href="style.css" rel="stylesheet" type="text/css" />
 	</head>
 
 	<header>
